e49af4500f2d7d33ff7931aeb1f4038c996f55a1
Ohi20 <ashrafohi20@gmail.com>
2024-02-19 15:30:59 +0600

    server-created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+
+//dotenv conig
+dotenv.config();
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//port
+const port = process.env.PORT || 8080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
